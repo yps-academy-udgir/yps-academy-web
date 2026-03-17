f70132a1762031baaa6de04910015561778de823
DineshPatil025 <dineshbpatil74@gmail.com>
2026-03-17 16:13:54 +0530

    faculty assignment is done

---
+++ frontend/src/app/features/classroom/services/faculty-assignment.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.dev';
+
+export interface FacultyAssignmentPayload {
+  facultyId: string;
+  subject: string;
+  isPrimary?: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class FacultyAssignmentService {
+    private API_URL = `${environment.apiUrl}/classrooms`;
+  
+
+  constructor(private http: HttpClient) {}
+
+  assignFaculty(classroomId: string, assignment: FacultyAssignmentPayload): Observable<any> {
+    return this.http.post(`${this.API_URL}/${classroomId}/faculty`, assignment);
+  }
+
+  removeFaculty(classroomId: string, facultyId: string, subject?: string): Observable<any> {
+    const url = `${this.API_URL}/${classroomId}/faculty/${facultyId}`;
+    const params = subject ? new HttpParams().set('subject', subject) : undefined;
+    return this.http.delete(url, params ? { params } : {});
+  }
+}
