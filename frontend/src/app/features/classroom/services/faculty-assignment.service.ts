09c09bffb6d209ce0fb00247e514ce81b5f058cc
DineshPatil025 <dineshbpatil74@gmail.com>
2026-03-18 15:36:28 +0530

    env file changes

--- frontend/src/app/features/classroom/services/faculty-assignment.service.ts
+++ frontend/src/app/features/classroom/services/faculty-assignment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment.dev';
+import { environment } from '../../../../environments/environment';
 
 export interface FacultyAssignmentPayload {
   facultyId: string;
