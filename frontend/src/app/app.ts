d56407e6fd80270796962b43aab5aae6330cc9bd
DineshPatil025 <dineshbpatil74@gmail.com>
2026-04-02 10:09:49 +0530

    theme changes

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('frontend');
+  private readonly themeService = inject(ThemeService);
 }
